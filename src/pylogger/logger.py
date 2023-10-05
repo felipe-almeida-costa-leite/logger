2bb975a514fe689fb2e4e6e9d3ebcf61551a2647
felipe.leite <felipecostasilvaleite@hotmail.com>
2023-10-05 11:13:19 -0300

    feat: Adicionando v0

--- src/pylogger/logger.py
+++ src/pylogger/logger.py
@@ -1,34 +1,34 @@
-import logging
+from logging import Logger as LogginLogger, Formatter, StreamHandler, INFO
 import sys
 from functools import partial, wraps
 from inspect import *
 from .spark_handler import SparkHandler
-from .spark_audit import SparkHandlerAudit
 from .sqs_handler import SQSHandler
 
 
-class Logger(object):
-    def __init__(self, name, level=logging.INFO, stream=sys.stdout,
-                 format="[%(levelname)s]|%(asctime)s|%(name)s|%(message)s"):
-        self.format = format
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(level)
-        self.stream = logging.StreamHandler(stream)
-        self.stream.setLevel(logging.INFO)
-        self.stream.setFormatter(fmt=logging.Formatter(self.format))
-        self.logger.addHandler(self.stream)
+class Logger(LogginLogger):
+    def __init__(self, name, level=INFO, stream=sys.stdout,
+                 format_logger="[%(levelname)s]|%(asctime)s|%(name)s|%(message)s"):
+        LogginLogger.__init__(self=self, name=name)
+        self.format = format_logger
+        self.level = level
+        self.setLevel(self.level)
+        self.stream = StreamHandler(stream)
+        self.stream.setLevel(INFO)
+        self.stream.setFormatter(fmt=Formatter(self.format))
+        self.addHandler(self.stream)
         self.__sparklog = False
         self.__auditlog = False
         self.__sqslog = False
 
     def add(self, handler, *args, **kwargs):
         new_handler = handler(*args, **kwargs)
-        new_handler.setLevel(logging.INFO)
-        new_handler.setFormatter(fmt=logging.Formatter(self.format))
-        self.logger.addHandler(new_handler)
+        new_handler.setLevel(self.level)
+        new_handler.setFormatter(fmt=Formatter(self.format))
+        self.addHandler(new_handler)
 
     def remove(self, handler):
-        self.logger.removeHandler(handler)
+        self.removeHandler(handler)
 
     def sparklog(self, *args, **kwargs):
         if self.__sparklog:
@@ -41,16 +41,6 @@ class Logger(object):
             self.add(handler=SparkHandler, *args, **kwargs)
             return self.__sparklog
 
-    def auditlog(self, *args, **kwargs):
-        if self.__auditlog:
-            self.__auditlog = False
-            self.remove(handler=SparkHandlerAudit)
-            return self.__auditlog
-        else:
-            self.__auditlog = True
-            self.add(handler=SparkHandlerAudit, *args, **kwargs)
-            return self.__auditlog
-
     def sqslog(self, *args, **kwargs):
         if self.__sqslog:
             self.__sqslog = False
@@ -60,18 +50,6 @@ class Logger(object):
             self.__sqslog = True
             self.add(handler=SQSHandler, *args, **kwargs)
             return self.__sqslog
-
-    def debug(self, msg: str):
-        self.logger.debug(msg)
-
-    def info(self, msg: str):
-        self.logger.info(msg)
-
-    def warning(self, msg: str):
-        self.logger.warning(msg)
-
-    def error(self, msg: str):
-        self.logger.error(msg)
 
     def __call__(self, func=None, rt=True, iargs=None, eargs=None, largs=True):
         if func is None:
@@ -91,7 +69,7 @@ class Logger(object):
                             "ArgName": f"{vargs}",
                             "ArgValue": f"{precalled[vargs]}"
                         }
-                        self.info(f"LogArgs: {str(message)}")
+                        self.info(f"Arg: {str(message)}")
                 if iargs:
                     for vargs in iargs:
                         message = {
@@ -99,7 +77,7 @@ class Logger(object):
                             "ArgName": f"{vargs}",
                             "ArgValue": f"{precalled[vargs]}"
                         }
-                        self.info(f"LogArgs: {str(message)}")
+                        self.info(f"Arg: {str(message)}")
                 if eargs:
                     set_precalled_keys = set(precalled.keys())
                     set_eargs = set(eargs)
@@ -110,23 +88,20 @@ class Logger(object):
                             "ArgName": f"{vargs}",
                             "ArgValue": f"{precalled[vargs]}"
                         }
-                        self.info(f"LogArgs: {str(message)}")
+                        self.info(f"Arg: {str(message)}")
                 response = func(*args, **kwargs)
                 if rt:
                     allargs = list(precalled.keys())
                     if 'self' in allargs:
                         pass
                     else:
-                        self.info(f"LogReturn: {response}")
+                        self.info(f"Return: {response}")
             except Exception as err:
                 self.error(f"{err}")
                 raise err
 
         return wrapper
 
-    def handlers(self):
-        return self.logger.handlers
-
     def close(self):
-        for handler in self.handlers():
+        for handler in self.handlers:
             self.remove(handler=handler)
--- src/test/test_logger.py
+++ src/test/test_logger.py
@@ -1,9 +1,112 @@
 import unittest
+from src.pylogger.logger import Logger
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class LoggerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger(__file__)
+
+    def tearDown(self) -> None:
+        self.logger.close()
+
+    def test_streamhandler(self):
+        found_stream = False
+        for handler in self.logger.handlers:
+            if "StreamHandler" in str(handler):
+                found_stream = True
+        self.assertTrue(found_stream)
+
+    def test_streamhandler_info(self):
+        messages = ["MessageTest1", "MessageTest2"]
+        with self.assertLogs(self.logger, level="INFO") as stream:
+            for message in messages:
+                self.logger.info(message)
+        for record in stream.records:
+            self.assertIn(record.getMessage(), messages)
+
+    def test_decorator_logger(self):
+        messages = [
+            "Arg: {'FuncName': 'main', 'ArgName': 'parameter', 'ArgValue': 'testparameter'}",
+            "MessageTestInner",
+            "Return: testparameter"
+        ]
+
+        @self.logger
+        def main(parameter):
+            self.logger.info("MessageTestInner")
+            return parameter
+
+        with self.assertLogs(self.logger, level="INFO") as stream:
+            main("testparameter")
+
+        for record in stream.records:
+            self.assertIn(record.getMessage(), messages)
+
+    def test_decorator_logger_nonrt(self):
+        messages = [
+            "Return: testparameter"
+        ]
+
+        @self.logger(rt=False)
+        def main(parameter):
+            self.logger.info("MessageTestInner")
+            return parameter
+
+        with self.assertLogs(self.logger, level="INFO") as stream:
+            main("testparameter")
+
+        for record in stream.records:
+            self.assertNotIn(record.getMessage(), messages)
+
+    def test_decorator_logger_nonlargs(self):
+        messages = [
+            "Arg: {'FuncName': 'main', 'ArgName': 'parameter', 'ArgValue': 'testparameter'}"
+        ]
+
+        @self.logger(largs=False)
+        def main(parameter):
+            self.logger.info("MessageTestInner")
+            return parameter
+
+        with self.assertLogs(self.logger, level="INFO") as stream:
+            main("testparameter")
+
+        for record in stream.records:
+            self.assertNotIn(record.getMessage(), messages)
+
+    def test_decorator_logger_iargs(self):
+        messages = [
+            "Arg: {'FuncName': 'main', 'ArgName': 'parameter', 'ArgValue': 'testparameter'}",
+            "MessageTestInner",
+            "Return: ('testparameter', 'testparameter2')"
+        ]
+
+        @self.logger(iargs=["parameter"])
+        def main(parameter, second):
+            self.logger.info("MessageTestInner")
+            return parameter, second
+
+        with self.assertLogs(self.logger, level="INFO") as stream:
+            main("testparameter", "testparameter2")
+
+        for record in stream.records:
+            self.assertIn(record.getMessage(), messages)
+
+    def test_decorator_logger_eargs(self):
+        messages = [
+            "Arg: {'FuncName': 'main', 'ArgName': 'parameter', 'ArgValue': 'testparameter'}",
+        ]
+
+        @self.logger(eargs=["parameter"])
+        def main(parameter, second):
+            self.logger.info("MessageTestInner")
+            return parameter, second
+
+        with self.assertLogs(self.logger, level="INFO") as stream:
+            main("testparameter", "testparameter2")
+
+        for record in stream.records:
+            self.assertNotIn(record.getMessage(), messages)
 
 
 if __name__ == '__main__':
